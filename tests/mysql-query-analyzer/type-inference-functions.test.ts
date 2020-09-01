4a996d08f21b743bced06ece0a884fe79712013b
Wagner Porto <wsporto.dev@gmail.com>
2020-08-31 21:16:01 -0300

    add suport to YEAR, MONTH and DAY functions

--- tests/mysql-query-analyzer/type-inference-functions.test.ts
+++ tests/mysql-query-analyzer/type-inference-functions.test.ts
@@ -5,13 +5,100 @@ import { TypeInferenceResult } from "../../src/mysql-query-analyzer/types";
 
 describe('type-inference - functions', () => {
 
-    it(`SELECT id FROM mytable1`, () => {
+    it(`SELECT NOW(), CURDATE(), CURTIME()`, () => {
         const sql = `SELECT NOW(), CURDATE(), CURTIME()`;
         const actual = parseAndInfer(sql, dbSchema);
 
         const expected : TypeInferenceResult = {
             columns: ['datetime', 'date', 'time'],
             parameters: []   
+        }
+
+        assert.deepEqual(actual, expected);
+    })
+
+    //The YEAR Type: As the result of functions that return a value that is acceptable in YEAR context, such as NOW().
+    //https://dev.mysql.com/doc/refman/8.0/en/year.html
+    //But the SQL binary protocol returns BIGINT
+    it(`SELECT YEAR(NOW()), YEAR(CURDATE())`, () => {
+        const sql = `SELECT YEAR(NOW()), YEAR(CURDATE())`;
+        const actual = parseAndInfer(sql, dbSchema);
+
+        const expected : TypeInferenceResult = {
+            columns: ['year', 'year'],
+            parameters: []   
+        }
+
+        assert.deepEqual(actual, expected);
+    })
+
+    it(`SELECT MONTH(NOW()), MONTH(CURDATE())`, () => {
+        const sql = `SELECT MONTH(NOW()), MONTH(CURDATE())`;
+        const actual = parseAndInfer(sql, dbSchema);
+
+        const expected : TypeInferenceResult = {
+            columns: ['tinyint', 'tinyint'],
+            parameters: []   
+        }
+
+        assert.deepEqual(actual, expected);
+    })
+
+    it(`SELECT DAY(NOW()), DAY(CURDATE())`, () => {
+        const sql = `SELECT DAY(NOW()), DAY(CURDATE())`;
+        const actual = parseAndInfer(sql, dbSchema);
+
+        const expected : TypeInferenceResult = {
+            columns: ['tinyint', 'tinyint'],
+            parameters: []   
+        }
+
+        assert.deepEqual(actual, expected);
+    })
+
+    it(`SELECT YEAR(CURTIME())`, () => {
+        const sql = `SELECT YEAR(CURTIME())`;
+        try {
+            parseAndInfer(sql, dbSchema);
+            assert.fail('Should throw an exception');
+        }
+        catch(e) {
+            const expectedMessage = 'Type mismatch: time and date';
+            assert.deepEqual(e.message, expectedMessage);
+        }
+    })
+
+    it(`SELECT MONTH(CURTIME())`, () => {
+        const sql = `SELECT MONTH(CURTIME())`;
+        try {
+            parseAndInfer(sql, dbSchema);
+            assert.fail('Should throw an exception');
+        }
+        catch(e) {
+            const expectedMessage = 'Type mismatch: time and date';
+            assert.deepEqual(e.message, expectedMessage);
+        }
+    })
+
+    it(`SELECT DAY(CURTIME())`, () => {
+        const sql = `SELECT DAY(CURTIME())`;
+        try {
+            parseAndInfer(sql, dbSchema);
+            assert.fail('Should throw an exception');
+        }
+        catch(e) {
+            const expectedMessage = 'Type mismatch: time and date';
+            assert.deepEqual(e.message, expectedMessage);
+        }
+    })
+
+    it(`SELECT YEAR(?), MONTH(?), DAY(?)`, () => {
+        const sql = `SELECT YEAR(?), MONTH(?), DAY(?)`;
+        const actual = parseAndInfer(sql, dbSchema);
+
+        const expected : TypeInferenceResult = {
+            columns: ['year', 'tinyint', 'tinyint'],
+            parameters: ['date', 'date', 'date']   
         }
 
         assert.deepEqual(actual, expected);
