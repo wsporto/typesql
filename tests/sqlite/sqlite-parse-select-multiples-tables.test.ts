2d349bf7dcb853be5218908048961b6f3bf3050e
Wagner Porto <wsporto.dev@gmail.com>
2024-04-07 18:14:56 -0300

    sqlite: add more tests

--- tests/sqlite/sqlite-parse-select-multiples-tables.test.ts
+++ tests/sqlite/sqlite-parse-select-multiples-tables.test.ts
@@ -147,4 +147,98 @@ describe.only('sqlite-parse-select-multiples-tables', () => {
 		}
 		assert.deepStrictEqual(actual.right, expected);
 	})
+
+	it('select t2.* from inner join', async () => {
+
+		const sql = `
+        SELECT t2.* 
+        FROM mytable1 t1 
+        INNER JOIN mytable2 t2 on t2.id = t1.id
+        `
+		const actual = await parseSql(sql, sqliteDbSchema);
+		const expected: SchemaDef = {
+			sql,
+			queryType: 'Select',
+			multipleRowsResult: true,
+			columns: [
+				{
+					columnName: 'id',
+					type: 'INTEGER',
+					notNull: true,
+					table: 'T2'
+				},
+				{
+					columnName: 'name',
+					type: 'TEXT',
+					notNull: false,
+					table: 'T2'
+				},
+				{
+					columnName: 'descr',
+					type: 'TEXT',
+					notNull: false,
+					table: 'T2'
+				}
+			],
+			parameters: []
+
+		}
+		if (isLeft(actual)) {
+			assert.fail(`Shouldn't return an error`);
+		}
+		assert.deepStrictEqual(actual.right, expected);
+	})
+
+	it('select t2.*, t1.* from inner join', async () => {
+
+		const sql = `
+        SELECT t2.*, t1.*
+        FROM mytable1 t1 
+        INNER JOIN mytable2 t2 on t2.id = t1.id
+        `
+		const actual = await parseSql(sql, sqliteDbSchema);
+		const expected: SchemaDef = {
+			sql,
+			queryType: 'Select',
+			multipleRowsResult: true,
+			columns: [
+				{
+					columnName: 'id',
+					type: 'INTEGER',
+					notNull: true,
+					table: 't2'
+				},
+				{
+					columnName: 'name',
+					type: 'TEXT',
+					notNull: false,
+					table: 'T2'
+				},
+				{
+					columnName: 'descr',
+					type: 'TEXT',
+					notNull: false,
+					table: 'T2'
+				},
+				{
+					columnName: 'id', //TODO - rename field
+					type: 'INTEGER',
+					notNull: true,
+					table: 'T1'
+				},
+				{
+					columnName: 'value',
+					type: 'INTEGER',
+					notNull: false,
+					table: 'T1'
+				},
+			],
+			parameters: []
+
+		}
+		if (isLeft(actual)) {
+			assert.fail(`Shouldn't return an error`);
+		}
+		assert.deepStrictEqual(actual.right, expected);
+	})
 });
