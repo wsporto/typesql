66e6bbbbb5354d49068927668a5001f0daa6511d
Wagner Porto <wsporto.dev@gmail.com>
2024-04-07 18:28:23 -0300

    sqlite: add test

--- tests/sqlite/sqlite-parse-select-multiples-tables.test.ts
+++ tests/sqlite/sqlite-parse-select-multiples-tables.test.ts
@@ -241,4 +241,40 @@ describe.only('sqlite-parse-select-multiples-tables', () => {
 		}
 		assert.deepStrictEqual(actual.right, expected);
 	})
+
+	it('parse select with param', async () => {
+
+		const sql = `
+        SELECT t1.id
+        FROM mytable1 t1 
+        INNER JOIN mytable2 t2 on t2.id = t1.id
+        WHERE t2.id = ?
+        `
+		const actual = await parseSql(sql, sqliteDbSchema);
+		const expected: SchemaDef = {
+			sql,
+			queryType: 'Select',
+			multipleRowsResult: true, //changed at v0.5.13
+			columns: [
+				{
+					columnName: 'id',
+					type: 'INTEGER',
+					notNull: true,
+					table: 'T1'
+				}
+			],
+			parameters: [
+				{
+					name: 'param1',
+					columnType: 'INTEGER',
+					notNull: true
+				}
+			]
+
+		}
+		if (isLeft(actual)) {
+			assert.fail(`Shouldn't return an error`);
+		}
+		assert.deepStrictEqual(actual.right, expected);
+	})
 });
