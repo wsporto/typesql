25db1b2ec90bfc1e647b3301e258fba5a42b95d7
Wagner Porto <wsporto.dev@gmail.com>
2024-04-07 21:05:14 -0300

    sqlite: add more tests for subqueries

--- tests/sqlite/sqlite-parse-select-subqueries.test.ts
+++ tests/sqlite/sqlite-parse-select-subqueries.test.ts
@@ -33,4 +33,125 @@ describe('sqlite-parse-select-subqueries', () => {
         }
         assert.deepStrictEqual(actual.right, expected);
     })
+
+    it('parse a select with nested select2', async () => {
+        const sql = `
+        select id, name from (
+            select t1.id, t2.name from mytable1 t1
+            inner join mytable2 t2 on t1.id = t2.id
+        ) t
+        `
+        const actual = await parseSql(sql, sqliteDbSchema);
+        const expected: SchemaDef = {
+            sql,
+            queryType: 'Select',
+            multipleRowsResult: true,
+            columns: [
+                {
+                    columnName: 'id',
+                    type: 'INTEGER',
+                    notNull: true,
+                    table: 'T'
+                },
+                {
+                    columnName: 'name',
+                    type: 'TEXT',
+                    notNull: false,
+                    table: 'T'
+                }
+            ],
+            parameters: []
+
+        }
+        if (isLeft(actual)) {
+            assert.fail(`Shouldn't return an error`);
+        }
+        assert.deepStrictEqual(actual.right, expected);
+    })
+
+    it('parse a select with nested select and alias', async () => {
+        const sql = `
+        select id from (
+            select value as id from mytable1
+        ) t1
+        `
+        const actual = await parseSql(sql, sqliteDbSchema);
+        const expected: SchemaDef = {
+            sql,
+            queryType: 'Select',
+            multipleRowsResult: true,
+            columns: [
+                {
+                    columnName: 'ID',
+                    type: 'INTEGER',
+                    notNull: false,
+                    table: 'T1'
+                }
+            ],
+            parameters: []
+
+        }
+        if (isLeft(actual)) {
+            assert.fail(`Shouldn't return an error`);
+        }
+        assert.deepStrictEqual(actual.right, expected);
+    })
+
+    it('parse a select with nested select and alias 2', async () => {
+        const sql = `
+        select id as code from (
+            select value as id from mytable1
+        ) t1
+        `
+        const actual = await parseSql(sql, sqliteDbSchema);
+        const expected: SchemaDef = {
+            sql,
+            queryType: 'Select',
+            multipleRowsResult: true,
+            columns: [
+                {
+                    columnName: 'CODE',
+                    type: 'INTEGER',
+                    notNull: false,
+                    table: 'T1'
+                }
+            ],
+            parameters: []
+
+        }
+        if (isLeft(actual)) {
+            assert.fail(`Shouldn't return an error`);
+        }
+        assert.deepStrictEqual(actual.right, expected);
+    })
+
+    it('parse a select with 3-levels nested select', async () => {
+        const sql = `
+        select id from (
+            select id from (
+                select id from mytable1 
+            ) t1
+        ) t2
+        `
+        const actual = await parseSql(sql, sqliteDbSchema);
+        const expected: SchemaDef = {
+            sql,
+            queryType: 'Select',
+            multipleRowsResult: true,
+            columns: [
+                {
+                    columnName: 'id',
+                    type: 'INTEGER',
+                    notNull: true,
+                    table: 'T2'
+                }
+            ],
+            parameters: []
+
+        }
+        if (isLeft(actual)) {
+            assert.fail(`Shouldn't return an error`);
+        }
+        assert.deepStrictEqual(actual.right, expected);
+    })
 });
