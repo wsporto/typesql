d75d123257cdfd0b43d558702dfd34cef6ff97f1
Wagner Porto <wsporto.dev@gmail.com>
2024-04-16 16:37:52 -0300

    sqlite: add more tests for insert statement

--- tests/sqlite/parse-insert.test.ts
+++ tests/sqlite/parse-insert.test.ts
@@ -1,5 +1,5 @@
 import assert from "assert";
-import { SchemaDef } from "../../src/types";
+import { ParameterDef, SchemaDef } from "../../src/types";
 import { isLeft } from "fp-ts/lib/Either";
 import { parseSql } from "../../src/sqlite-query-analyzer/parser";
 import { sqliteDbSchema } from "../mysql-query-analyzer/create-schema";
@@ -39,5 +39,125 @@ describe('sqlite-parse-insert', () => {
             assert.fail(`Shouldn't return an error`);
         }
         assert.deepStrictEqual(actual.right, expected);
+    })
+
+    it('insert into mydb.mytable1 (value) values (?)', () => {
+
+        const sql = `
+        insert into mydb.mytable1 (value) values (?)
+            `;
+        const actual = parseSql(sql, sqliteDbSchema);
+        const expected: ParameterDef[] = [
+            {
+                name: 'param1',
+                columnType: 'INTEGER',
+                notNull: false
+            }
+        ]
+
+        if (isLeft(actual)) {
+            assert.fail(`Shouldn't return an error`);
+        }
+        assert.deepStrictEqual(actual.right.parameters, expected);
+    })
+
+    it('insert into mytable3 (name, double_value) values (?, ?)', () => {
+
+        const sql = `
+        insert into mytable3 (name, double_value) values (?, ?)
+            `;
+        const actual = parseSql(sql, sqliteDbSchema);
+        const expected: ParameterDef[] = [
+            {
+                name: 'param1',
+                columnType: 'TEXT',
+                notNull: true
+            },
+            {
+                name: 'param2',
+                columnType: 'REAL',
+                notNull: false
+            }
+        ]
+
+        if (isLeft(actual)) {
+            assert.fail(`Shouldn't return an error`);
+        }
+        assert.deepStrictEqual(actual.right.parameters, expected);
+    })
+
+    it('insert into mytable3 (double_value, name) values (?, ?)', () => {
+
+        const sql = `
+        insert into mytable3 (double_value, name) values (?, ?)
+            `;
+        const actual = parseSql(sql, sqliteDbSchema);
+        const expected: ParameterDef[] = [
+            {
+                name: 'param1',
+                columnType: 'REAL',
+                notNull: false
+            },
+            {
+                name: 'param2',
+                columnType: 'TEXT',
+                notNull: true
+            }
+        ]
+
+        if (isLeft(actual)) {
+            assert.fail(`Shouldn't return an error`);
+        }
+        assert.deepStrictEqual(actual.right.parameters, expected);
+    })
+
+    it('insert into mytable3 (name, double_value) values (:fullname, :value)', () => {
+
+        const sql = `
+        insert into mytable3 (name, double_value) values (:fullname, :value)
+            `;
+        const actual = parseSql(sql, sqliteDbSchema);
+        const expected: ParameterDef[] = [
+            {
+                name: 'fullname',
+                columnType: 'TEXT',
+                notNull: true
+            },
+            {
+                name: 'value',
+                columnType: 'REAL',
+                notNull: false
+            }
+        ]
+
+        if (isLeft(actual)) {
+            assert.fail(`Shouldn't return an error`);
+        }
+        assert.deepStrictEqual(actual.right.parameters, expected);
+    })
+
+    it('insert same parameter into two fields', () => {
+
+        const sql = `
+        insert into mytable2 (name, descr) values (:name, :name)
+            `;
+        const actual = parseSql(sql, sqliteDbSchema);
+        const expected: ParameterDef[] = [
+            {
+                name: 'name',
+                columnType: 'TEXT',
+                notNull: false
+            },
+            {
+                name: 'name',
+                columnType: 'TEXT',
+                notNull: false
+            }
+        ]
+
+        if (isLeft(actual)) {
+            assert.fail(`Shouldn't return an error`);
+        }
+        assert.deepStrictEqual(actual.right.parameters, expected);
     })
 });
