792e465fb3fb104ba8a2530153f81677e056b5db
Wagner Porto <wsporto.dev@gmail.com>
2024-04-25 19:20:37 -0300

    sqlite - add more tests for nested query result

--- tests/sqlite/sqlite-nested-query.test.ts
+++ tests/sqlite/sqlite-nested-query.test.ts
@@ -411,4 +411,185 @@ describe('sqlite-nested-query', () => {
 
 		assert.deepStrictEqual(actual.right.nestedInfo, expectedModel);
 	})
+
+	it('SELECT FROM users u INNER JOIN posts p INNER JOIN roles r INNER JOIN comments c', () => {
+
+		const sql = `
+		-- @nested
+        SELECT
+            u.id as user_id,
+            u.name as user_name,
+            p.id as post_id,
+            p.title as post_title,
+            p.body  as post_body,
+            r.role,
+            c.comment
+        FROM users u
+        INNER JOIN posts p on p.fk_user = u.id
+        INNER JOIN roles r on r.fk_user = u.id
+        INNER JOIN comments c on c.fk_post = p.id
+        `
+		// Expected types:
+		// type User = {
+		//     user_id: string;
+		//     user_name: string;
+		//     p: Post[];
+		//     r: Role[];
+		// }
+		// type Post = {
+		//     post_id: string;
+		//     post_title: string;
+		//     post_body: string;
+		//     c: Comment[];
+		// }
+
+		const expectedModel: RelationInfo2[] = [
+			{
+				name: 'users',
+				alias: 'u',
+				fields: [
+					{
+						name: 'user_id',
+						index: 0
+					},
+					{
+						name: 'user_name',
+						index: 1
+					}
+				],
+				relations: [
+					{
+						name: 'posts',
+						alias: 'p',
+						cardinality: 'many'
+					},
+					{
+						name: 'roles',
+						alias: 'r',
+						cardinality: 'many'
+					}
+				]
+			},
+			{
+				name: 'posts',
+				alias: 'p',
+				fields: [
+					{
+						name: 'post_id',
+						index: 2
+					},
+					{
+						name: 'post_title',
+						index: 3
+					},
+					{
+						name: 'post_body',
+						index: 4
+					}
+				],
+				relations: [
+					{
+						name: 'comments',
+						alias: 'c',
+						cardinality: 'many'
+					}
+				]
+			},
+			{
+				name: 'roles',
+				alias: 'r',
+				fields: [
+					{
+						name: 'role',
+						index: 5
+					}
+				],
+				relations: []
+			},
+			{
+				name: 'comments',
+				alias: 'c',
+				fields: [
+					{
+						name: 'comment',
+						index: 6
+					}
+				],
+				relations: []
+			}
+		]
+
+		const actual = parseSql(sql, sqliteDbSchema);
+		if (isLeft(actual)) {
+			assert.fail(`Shouldn't return an error`);
+		}
+
+		assert.deepStrictEqual(actual.right.nestedInfo, expectedModel);
+	})
+
+	it('SELECT FROM users u INNER JOIN posts p', () => {
+
+		const sql = `
+		-- @nested
+        SELECT
+            *
+        FROM users u
+        INNER JOIN posts p on p.fk_user = u.id
+        `
+		//[id(0), name, id(2), title, body, fk_user]
+
+		const expectedModel: RelationInfo2[] = [
+			{
+				name: 'users',
+				alias: 'u',
+				fields: [
+					{
+						name: 'id',
+						index: 0
+					},
+					{
+						name: 'name',
+						index: 1
+					}
+				],
+				relations: [
+					{
+						name: 'posts',
+						alias: 'p',
+						cardinality: 'many',
+					}
+				]
+			},
+			{
+				name: 'posts',
+				alias: 'p',
+				fields: [
+					{
+						name: 'id',
+						index: 2
+					},
+					{
+						name: 'title',
+						index: 3
+					},
+					{
+						name: 'body',
+						index: 4
+					},
+					{
+						name: "fk_user",
+						index: 5,
+					}
+				],
+				relations: []
+			}
+		]
+
+		const actual = parseSql(sql, sqliteDbSchema);
+		if (isLeft(actual)) {
+			assert.fail(`Shouldn't return an error`);
+		}
+
+		assert.deepStrictEqual(actual.right.nestedInfo, expectedModel);
+	})
 })
