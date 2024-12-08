b6b211f83b807ef5df40bb7d2d6b03be35b8c242
Wagner Porto <wsporto.dev@gmail.com>
2024-12-08 14:03:25 -0300

    add more tests

---
+++ tests/postgres/parse-select-subqueries.test.ts
@@ -0,0 +1,162 @@
+import assert from 'node:assert';
+import type { SchemaDef } from '../../src/types';
+import postgres from 'postgres';
+import { describeQuery } from '../../src/postgres-query-analyzer/describe';
+
+describe('postgres-parse-select-subqueries', () => {
+	const postres = postgres({
+		host: 'localhost',
+		username: 'postgres',
+		password: 'password',
+		database: 'postgres',
+		port: 5432
+	});
+
+	it('parse a select with nested select', async () => {
+		const sql = `
+        select id from (
+            select id from mytable1
+        ) t
+        `;
+		const actual = await describeQuery(postres, sql, []);
+		const expected: SchemaDef = {
+			sql,
+			queryType: 'Select',
+			multipleRowsResult: true,
+			columns: [
+				{
+					columnName: 'id',
+					type: 'int4',
+					notNull: true,
+					table: 'table'
+				}
+			],
+			parameters: []
+		};
+		if (actual.isErr()) {
+			assert.fail(`Shouldn't return an error: ${actual.error.description}`);
+		}
+		assert.deepStrictEqual(actual.value, expected);
+	});
+
+	it('parse a select with nested select2', async () => {
+		const sql = `
+        select id, name from (
+            select t1.id, t2.name from mytable1 t1
+            inner join mytable2 t2 on t1.id = t2.id
+        ) t
+        `;
+		const actual = await describeQuery(postres, sql, []);
+		const expected: SchemaDef = {
+			sql,
+			queryType: 'Select',
+			multipleRowsResult: true,
+			columns: [
+				{
+					columnName: 'id',
+					type: 'int4',
+					notNull: true,
+					table: 'table'
+				},
+				{
+					columnName: 'name',
+					type: 'text',
+					notNull: false,
+					table: 'table'
+				}
+			],
+			parameters: []
+		};
+		if (actual.isErr()) {
+			assert.fail(`Shouldn't return an error: ${actual.error.description}`);
+		}
+		assert.deepStrictEqual(actual.value, expected);
+	});
+
+	it('parse a select with nested select and alias', async () => {
+		const sql = `
+        select id from (
+            select value as id from mytable1
+        ) t1
+        `;
+		const actual = await describeQuery(postres, sql, []);
+		const expected: SchemaDef = {
+			sql,
+			queryType: 'Select',
+			multipleRowsResult: true,
+			columns: [
+				{
+					columnName: 'id',
+					type: 'int4',
+					notNull: false,
+					table: 'table'
+				}
+			],
+			parameters: []
+		};
+		if (actual.isErr()) {
+			assert.fail(`Shouldn't return an error: ${actual.error.description}`);
+		}
+		assert.deepStrictEqual(actual.value, expected);
+	});
+
+	it('parse a select with nested select and alias 2', async () => {
+		const sql = `
+        select id as code from (
+            select value as id from mytable1
+        ) t1
+        `;
+		const actual = await describeQuery(postres, sql, []);
+		const expected: SchemaDef = {
+			sql,
+			queryType: 'Select',
+			multipleRowsResult: true,
+			columns: [
+				{
+					columnName: 'code',
+					type: 'int4',
+					notNull: false,
+					table: 'table'
+				}
+			],
+			parameters: []
+		};
+		if (actual.isErr()) {
+			assert.fail(`Shouldn't return an error: ${actual.error.description}`);
+		}
+		assert.deepStrictEqual(actual.value, expected);
+	});
+
+	it('select * from (subquery)', async () => {
+		const sql = `
+        select * from (
+            select name, name as id from mytable2
+        ) t2
+        `;
+		const actual = await describeQuery(postres, sql, []);
+		const expected: SchemaDef = {
+			sql,
+			queryType: 'Select',
+			multipleRowsResult: true,
+			columns: [
+				{
+					columnName: 'name',
+					type: 'text',
+					notNull: false,
+					table: 'table'
+				},
+				{
+					columnName: 'id',
+					type: 'text',
+					notNull: false,
+					table: 'table'
+				}
+			],
+			parameters: []
+		};
+		if (actual.isErr()) {
+			assert.fail(`Shouldn't return an error: ${actual.error.description}`);
+		}
+		assert.deepStrictEqual(actual.value, expected);
+	});
+});
