26eafb845a2e691ae0530d88006e26ddf9825c0a
Wagner Porto <wsporto.dev@gmail.com>
2025-07-19 22:49:16 -0300

    test(postgres): add e2e test for user function call

---
+++ tests/postgres-e2e/sql/select-user-function01.ts
@@ -0,0 +1,22 @@
+import pg from 'pg';
+
+export type SelectUserFunction01Result = {
+	id: number;
+	value?: number;
+}
+
+export async function selectUserFunction01(client: pg.Client | pg.Pool): Promise<SelectUserFunction01Result[]> {
+	const sql = `
+	SELECT * FROM get_mytable1()
+	`
+	return client.query({ text: sql, rowMode: 'array' })
+		.then(res => res.rows.map(row => mapArrayToSelectUserFunction01Result(row)));
+}
+
+function mapArrayToSelectUserFunction01Result(data: any) {
+	const result: SelectUserFunction01Result = {
+		id: data[0],
+		value: data[1]
+	}
+	return result;
+}
